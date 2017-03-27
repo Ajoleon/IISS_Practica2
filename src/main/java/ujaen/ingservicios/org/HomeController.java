5abbbdef552388de60c2fe4ad5596749c521cb36
DanielMesaGonzalez <dmg00040@red.ujaen.es>
2017-03-27 11:28:16 +0200

    aaaa

--- src/main/java/ujaen/ingservicios/org/HomeController.java
+++ src/main/java/ujaen/ingservicios/org/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
+/***
  * Handles requests for the application home page.
  */
 @Controller
@@ -46,8 +46,8 @@ public class HomeController {
 	
 	/*A partir de aqui lo hemos empezado a implementar nosotros*/
 	
-	@RequestMapping(value = "/Aquiponernombre", method = RequestMethod.POST)
-	public String aquiponernombre(HttpServletRequest req, Model mod) {
+	@RequestMapping(value = "/Usuario", method = RequestMethod.POST)
+	public String Usuario(HttpServletRequest req, Model mod) {
 		String user = req.getParameter("Usuario");
 	    String email = req.getParameter("Email");
 	    String dir = req.getParameter("Direccion");
