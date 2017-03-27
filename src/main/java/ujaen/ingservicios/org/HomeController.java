c5fa737ea75d1a2abe97c975430fb12f01ac46a4
Antonio José León Sánchez <antonioleon@MacBook-Pro-de-Antonio.local>
2017-03-27 12:24:52 +0200

    Avances para registro

--- src/main/java/ujaen/ingservicios/org/HomeController.java
+++ src/main/java/ujaen/ingservicios/org/HomeController.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /***
  * Handles requests for the application home page.
@@ -38,19 +41,58 @@ public class HomeController {
 		String emailAddress = "";
 		if(cookies!=null){
 			for(Cookie cookie:cookies){
-				
+				if (cookieName.equals(cookie.getName())) 
+					emailAddress = cookie.getValue();
 			}
 		}
+		if(emailAddress.equals("")){
+			//Busco base de datos usuario
+			return "shop";
+		}else{
+			return "index";
+		}
 		
-		return "index";
 	}
 	@RequestMapping(value = "/Registro", method = RequestMethod.GET)
-	public String Registro(Locale locale, Model model) {
-		
-		
-		return "registro";
+	public String Registro(HttpServletRequest req,HttpServletResponse res, Model model) {
+		String user = req.getParameter("user");
+		String pass = req.getParameter("pass");
+		if(user.equals("admin") && pass.equals("admin")){
+			HttpSession session = req.getSession(true);
+			session.setAttribute("name","administrador");
+			
+			return "Admin";
+		}else{ 
+			//buscar el base de datos
+			//si existe, devuelvo shop, si no, sigo igual
+			return "Registro";
+		}
 	}
-	
+	@RequestMapping(value = "/Shop", method = RequestMethod.GET)
+	public String Shop(HttpServletRequest req, Model model) {
+
+		return "shop";
+	}
+	@RequestMapping(value = "/Nuevo", method = RequestMethod.GET)
+	public String Nuevo(HttpServletRequest req,HttpServletResponse res, Model model) {
+		String user = req.getParameter("nombre");
+		String pass =req.getParameter("user");
+		String tlf = req.getParameter("tlf");
+		String dir = req.getParameter("dir");
+		String email = req.getParameter("email");
+		HttpSession session = req.getSession(true );
+		Cookie c = new Cookie("emailCookie", email); 
+		c.setMaxAge(60*60*24*365*2);
+		c.setPath("/org/");
+		res.addCookie(c);
+		UsuarioDTO usuario  = new UsuarioDTO(user,pass,email,dir,tlf);
+		//buscar en base de datos si existe con email, si no
+		//almacenar en base de datos
+		//si existe, devuelvo Nuevo de nuevo
+		session.setAttribute("usuario", usuario);
+		session.setAttribute("total", 0);
+		return "shop";
+	}
 	
 	
 }
