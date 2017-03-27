267475f6fad7d4a80daed9ac3f189b3e5b015867
DanielMesaGonzalez <dmg00040@red.ujaen.es>
2017-03-27 12:23:52 +0200

    implementación clases nuevo,leernombre,leeremail.. usuarioDAOjdbc

--- src/main/java/ujaen/ingservicios/org/UsuarioDAOjdbc.java
+++ src/main/java/ujaen/ingservicios/org/UsuarioDAOjdbc.java
@@ -1,18 +1,34 @@
 package ujaen.ingservicios.org;
 
 public class UsuarioDAOjdbc implements UsuarioDAO {
-
-	public List<Usuario> leeUsuarios(){
-		
-		/*CONEXION CON LA BASE DE DATOS*/
-		String sql= "select * from usuarios";
-		UsuarioMapper mapper= new UsuarioMapper();
-		List<Usuario> usuarios= this.jdbcTemplate.query(sql, mapper);
-		return usuarios;
-		
-	}
     
-	public String muestraJSP(){
-		return "muestra JSP";
+	public void NuevoUsuario(Usuario usuario){
+		String sql = "insert into usuarios values(?,?,?,?)";
+		Object[] parametros = {usuario.getNombre(),usuario.getEmail(),usuario.getDireccion(),usuario.get.Telefono()};
+		this.jdbcTemplate.update(sql,parametros);
+		}
+	
+	public Usuario LeerEmail(int Email){ //Devuelve el usuario buscado o null si no existe
+		String sql = "select * from usuarios where Email = ?";
+		Object[ ] parametros = {Email}; //Array de objetos
+		UsuarioMapper mapper = new UsuarioMapper();
+		List<Usuario> usuarios = this.jdbcTemplate.query(sql, parametros, mapper);
+		if (usuarios.isEmpty()) return null;
+		else return usuarios.get(0);
+		}
+	public Usuario LeerNombre(int Nombre){
+		String sql = "select * from usuarios where Nombre = ?";
+		Object[ ] parametros = {Nombre}; 
+		UsuarioMapper mapper = new UsuarioMapper();
+		List<Usuario> usuarios = this.jdbcTemplate.query(sql, parametros, mapper);
+		if (usuarios.isEmpty()) return null;
+		else return usuarios.get(0);
 	}
+	
+	public List<Usuario> LeerTodos(){
+		String sql = "select * from usuarios";
+		UsuarioMapper mapper = new UsuarioMapper();
+		List<Usuario> usuarios = this.jdbcTemplate.query(sql, mapper);
+		return usuarios;
+		}
 }
